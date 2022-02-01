8770d5d3d04e0cb4127ef603d5f4b249a5d4f712
MEKL20 <edwinkhoirudin16@gmail.com>
2022-02-02 02:06:31 +0700

    fix

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -4,7 +4,6 @@ module.exports = {
     sendMail : (req, res) => {
         const { name, email, subject, message } = req.body
         const transporter = nodemailer.createTransport({
-            // service: "gmail",
             host: "smtp.gmail.com",
             port: 465,
             secure: true,
@@ -27,42 +26,32 @@ module.exports = {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <title>Document</title>
                 
-                    <style>
-                    body {
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                    }
-                    .header {
-                        text-align: center;
-                    }
-                
-                    .body-message {
-                        background-color: salmon;
-                        display: block;
-                        justify-content: center;
-                        width: fit-content;
-                        padding: 24px 56px;
-                    }
-                    </style>
+                    <link
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                    rel="stylesheet"
+                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                    crossorigin="anonymous"
+                    />
                 </head>
                 <body>
-                    <div class="header">
-                    <h1>Hello World!</h1>
+                    <div class="container d-flex justify-content-center">
+                    <div class="card bg-warning my-5 w-100" style="max-width: 600px">
+                        <div class="card-body">
+                        <h5 class="card-title mb-3">Glints Talk</h5>
+                        <h6 class="card-subtitle mb-2">from : ${name}</h6>
+                        <h6 class="card-subtitle mb-5">email : ${email}</h6>
+                        <p class="card-text">
+                            ${message}
+                        </p>
+                        </div>
+                    </div>
                     </div>
                 
-                    <div class="body-message">
-                    <h2>
-                        Welcome
-                        <br>
-                        to Glints Talk!
-                    </h2><br>
-                    <h4>
-                    Nama : ${name} <br>
-                    Email : ${email} <br>
-                    Message : ${message} <br>
-                    </h4>
-                    </div>
+                    <script
+                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                    crossorigin="anonymous"
+                    ></script>
                 </body>
                 </html>`
         }
